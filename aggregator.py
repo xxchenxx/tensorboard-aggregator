fb406c2233c9cd7688acbd5c501f12fd1a10b7d5
Ch4nYH <hfercxx@hotmail.com>
2021-06-13 11:58:27 +0800

    update

--- aggregator.py
+++ aggregator.py
@@ -45,7 +45,7 @@ def extract(dpath, subpath):
 
     for all_scalar_events in all_scalar_events_per_key:
         for scalar_events in all_scalar_events:
-            print(scalar_events.wall_time)
+            print(scalar_events)
     # Get and average wall times per step per key
     wall_times_per_key = [np.mean([tuple(scalar_event.wall_time for scalar_event in scalar_events) for scalar_events in all_scalar_events], axis=0)
                           for all_scalar_events in all_scalar_events_per_key]
